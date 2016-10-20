e8c2e4ceb8fc2077d74ac4c5d54208e837c2be03
Frankie Gavin and Steve Burton <frankie@>
2016-10-20 15:18:42 -0700

    add if and if else to JS.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,32 @@ $(document).ready(function() {
   $("form#questions").submit(function(event) {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
-    var genderPref = $("s")
-    var quote = (100 - age) * 3;
+    var genderPref = $("select#genderPref").val();
+    //var result = $(".A-H").val();
 
-    $("#rate").empty().append(quote);
-    $("#quote").show();
+  var celebrity = function(age, gender, genderPref){
+
+      if (age < 25 && gender === "male" && genderPref === "female") {
+        return ("A");
+      } else if (age <  25 && gender === "male" && genderPref === "male") {
+        return ("B");
+      } else if (age < 25 && gender === "female" && genderPref === "male") {
+        return ("C");
+      } else if (age < 25 && gender === "female" && genderPref === "female") {
+        return ("D");
+      } else if (age > 25 && gender === "male" && genderPref === "female") {
+        return ("E");
+      } else if (age > 25 && gender === "male" && genderPref === "male") {
+        return ("F");
+      } else if (age > 25 && gender === "female" && genderPref === "male") {
+        return ("G");
+      } else if (age > 25 && gender === "female" && genderPref === "female") {
+        return ("H");
+      }
+    };
+
+    $("#output").text(celebrity);
+    $("#yourceleb").show();
 
     event.preventDefault();
   });
